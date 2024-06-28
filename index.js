a53838d95e72cbf118928b1cab94475e3a51fa98
jummun513 <jummunislam513@gmail.com>
2024-06-28 23:15:26 +0600

    add payment integration

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { upload, sendImageToImageKit } = require('./imageUploadHandle');
+const SSLCommerzPayment = require('sslcommerz-lts');
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +11,11 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const store_id = process.env.SSL_STORE_ID
+const store_passwd = process.env.SSL_STORE_PASSWORD
+const is_live = false
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.03hem.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,6 +38,7 @@ async function run() {
         const usersCollection = client.db("Builder_Tools").collection("users");
         const blogsCollection = client.db("Builder_Tools").collection("blogs");
         const ordersCollection = client.db("Builder_Tools").collection("orders");
+        const reviewsCollection = client.db("Builder_Tools").collection("reviews");
 
         // jwt token create and pass for general user create and login
         app.post('/jwt', async (req, res) => {
@@ -187,6 +194,9 @@ async function run() {
             res.send(result);
         });
 
+
+
+        // order section
         app.post('/api/v1/order', async (req, res) => {
             try {
                 const data = req.body;
@@ -246,6 +256,88 @@ async function run() {
             } catch (error) {
                 res.status(500).send({ message: 'Server error', error: error.message });
             }
+        });
+
+        app.post('/api/v1/order/payment', async (req, res) => {
+            const data = req.body;
+            const tranId = new ObjectId().toString();
+            const order = await ordersCollection.findOne({ _id: new ObjectId(`${data.id}`) });
+            if (order) {
+                const tool = await toolsDataCollection.findOne({ _id: new ObjectId(order.toolId) });
+                order.toolId = tool;
+            }
+            console.log(order);
+            try {
+                //     const data = {
+                //         total_amount: 100,
+                //         currency: 'BDT',
+                //         tran_id: 'REF123',
+                //         success_url: 'http://localhost:3030/success',
+                //         fail_url: 'http://localhost:3030/fail',
+                //         cancel_url: 'http://localhost:3030/cancel',
+                //         ipn_url: 'http://localhost:3030/ipn',
+                //         shipping_method: 'Courier',
+                //         product_name: 'Computer.',
+                //         product_category: 'Electronic',
+                //         product_profile: 'general',
+                //         cus_name: 'Customer Name',
+                //         cus_email: 'customer@example.com',
+                //         cus_add1: 'Dhaka',
+                //         cus_add2: 'Dhaka',
+                //         cus_city: 'Dhaka',
+                //         cus_state: 'Dhaka',
+                //         cus_postcode: '1000',
+                //         cus_country: 'Bangladesh',
+                //         cus_phone: '01711111111',
+                //         cus_fax: '01711111111',
+                //         ship_name: 'Customer Name',
+                //         ship_add1: 'Dhaka',
+                //         ship_add2: 'Dhaka',
+                //         ship_city: 'Dhaka',
+                //         ship_state: 'Dhaka',
+                //         ship_postcode: 1000,
+                //         ship_country: 'Bangladesh',
+                //     };
+                //     const sslcz = new SSLCommerzPayment(store_id, store_passwd, is_live)
+                //     sslcz.init(data).then(apiResponse => {
+                //         // Redirect the user to payment gateway
+                //         let GatewayPageURL = apiResponse.GatewayPageURL
+                //         res.redirect(GatewayPageURL)
+                //         console.log('Redirecting to: ', GatewayPageURL)
+                //     });
+            } catch (error) {
+                res.status(500).send({ message: 'Server error', error: error.message });
+            }
+        });
+
+
+
+        // review section
+        app.post('/api/v1/review', async (req, res) => {
+            try {
+                const data = req.body;
+                await reviewsCollection.insertOne({ ...req.body, isDeleted: false });
+                res.status(201).json({
+                    success: true,
+                    message: 'Successfully added review'
+                });
+
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({
+                    success: false,
+                    message: 'Failed to add review',
+                    error: {
+                        code: 500,
+                        description: error?.message,
+                    }
+                });
+            }
+        });
+
+        app.get('/api/v1/reviews', async (req, res) => {
+            const result = await reviewsCollection.find().toArray();
+            res.send(result);
         });
     }
 
